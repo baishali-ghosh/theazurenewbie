110d01cff7f4bff7478a9ef7879956afbb620177
Baishali Ghosh <baishali13@gmail.com>
2016-10-18 13:27:23 +0530

    Sample solution to show how to write to a blob

---
+++ Blob/WriteToBlob/src/WriteToBlob/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace WriteToBlob
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            String result = "";
+            result = WriteBlob();
+            //Console.WriteLine(result);
+        }
+        public static string WriteBlob()
+        {
+            string accountName = "ENTER_YOUR_STORAGE_ACCOUNT_NAME";
+            string accountKey = "YOUR_STORAGE_ACCOUNT_KEY";
+	    string response = "";
+            try
+            {
+
+                StorageCredentials creds = new StorageCredentials(accountName, accountKey);
+                CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
+
+                CloudBlobClient client = account.CreateCloudBlobClient();
+
+                CloudBlobContainer sampleContainer = client.GetContainerReference("CONTAINER_NAME");
+                sampleContainer.CreateIfNotExists();
+
+                CloudBlockBlob blob = sampleContainer.GetBlockBlobReference("NAME_TO_GIVE_TO_UPLOADED_IMAGE");
+                using (Stream file = System.IO.File.OpenRead("LOCAL_PATH_OF_FILE_TO_UPLOAD"))
+                {
+                    blob.UploadFromStream(file);
+                }
+		response = "File uploaded successfully";
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex);
+		response = "An error occurred";            
+            }
+	    return response;
+            
+        }
+    }
+}
---
+++ Blob/WriteToBlob/src/WriteToBlob/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WriteToBlob")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WriteToBlob")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6d41f220-121d-4cdd-b101-77af20e82407")]
